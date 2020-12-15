d40012b604003deb94f68d1cc4fb3860968f01f6
Dionei Bianchati <dion.biancha@gmail.com>
2020-12-15 20:39:22 -0300

    Input area

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
 import './App.css';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+const Input = styled.input `
+  width: 400px;
+  height: 30px;
+`;
 
 function App() {
+
+  const [ valueCheck, setValueCheck] = useState('');
+
+  const checkInput = (e) => {
+    setValueCheck(e.target.valueCheck);
+  };
+
+  const [ valueTip, setValueTip] = useState('');
+
+  const tipInput = (e) => {
+    setValueTip(e.target.valueTip);
+  };
+
   return (
     <div className="App">
+      <h1>Tip Calculator</h1>
 
+      <p>How much did the bill give?</p>
+      <Input placeholder="Digite aqui" type="text" value={valueCheck} onChange={checkInput}/>
+
+      <p>What is the percentage of the tip?</p>
+      <Input placeholder="Digite aqui" type="text" value={valueTip} onChange={tipInput}/>
+
+      <p>Sub Total: </p>
+      <p>Tip:</p>
+      <p>Total:</p>
     </div>
   );
 }
